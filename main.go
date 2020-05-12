a456eff400c557173b0f8fdf710aefadc45ec0a3
Bridget McErlean <bmcerlean@vmware.com>
2020-05-11 21:05:42 -0400

    Use same calls as in sonobuoy

--- main.go
+++ main.go
@@ -2,24 +2,52 @@ package main
 
 import (
 	"fmt"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/restmapper"
 	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+func GetRESTConfig() (*rest.Config, error) {
+	cfgLoadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(cfgLoadingRules, configOverrides)
+	return kubeConfig.ClientConfig()
+}
+
 func main() {
-	config, err := rest.InClusterConfig()
+	config, err := GetRESTConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+
+	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println("could not create discovery client", err)
+		return
 	}
+
+	discover, err := discovery.NewDiscoveryClientForConfig(config)
+	if err != nil {
+		fmt.Println("could not create discovery client", err)
+		return
+	}
+
+	groupResources, err := restmapper.GetAPIGroupResources(discover)
+	if err != nil {
+		fmt.Println("could not get api group resources", err)
+		return
+	}
+
+	fmt.Printf("found %v API group resources\n", len(groupResources))
+
+
 	ns := os.Getenv("POD_NAMESPACE")
-	pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{})
+	pods, err := client.CoreV1().Pods(ns).List(metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
